f5c4a78efd7edf7229e1636304ef7786462625b7
Woliul Hasan <hmwoliul@gmail.com>
2019-02-24 20:22:42 +0600

    Fundamental-Syntax

---
+++ Fundamental/syntax.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright (c) 2019. All rights reserved by Woliul Hasan. Fork me on https://github.com/hmwoliul
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: Woliul Hasan
+ * Date: 24-Feb-19
+ * Time: 8:16 PM
+ */
